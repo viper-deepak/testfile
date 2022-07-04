1382f1d1b66d9b7aca5863a3db7b90ed8895ee05
Deepak Kumar D <deepakkumar.d@outlook.com>
2022-07-04 17:39:19 +0530

    after trying book restx example

--- appdb.py
+++
@@ -1,5 +0,0 @@
-import urllib
-from sqlalchemy import create_engine
-
-params = urllib.parse.quote_plus('Driver={SQL Server};Server=.;Database=sample;uid=sa;pwd=Admin@12345;')
-engine = create_engine("pypyodbc.connect(%s)" % params)
